b2131751f0cac543e251583b8cb3d206da1d0188
Jason Kroll <krolly@gmail.com>
2016-10-22 12:59:44 +1000

    Comments

--- migrate-database.sh
+++ migrate-database.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Executing bundle exec 'rake db:migrate' ..."
+echo "Executing bundle exec 'rake db:create' ..."
 RAILS_ENV=production bundle exec rake db:create
+echo "Executing bundle exec 'rake db:migrate' ..."
 RAILS_ENV=production bundle exec rake db:migrate
